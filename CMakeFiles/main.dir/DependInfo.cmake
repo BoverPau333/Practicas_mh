3997ad13a1f01ab5b8e85787b2b9155304db2fda
BoverPau333 <pauboverfemenais@gmail.com>
2025-03-13 17:42:23 +0100

    Cambiado el Cmake.txt

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/paubover/Escritorio/2dCuatri/Practicas_mh/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/paubover/Escritorio/2dCuatri/Practicas_mh/src/brutesearch.cpp" "CMakeFiles/main.dir/src/brutesearch.cpp.o" "gcc" "CMakeFiles/main.dir/src/brutesearch.cpp.o.d"
+  "/home/paubover/Escritorio/2dCuatri/Practicas_mh/src/greedy.cpp" "CMakeFiles/main.dir/src/greedy.cpp.o" "gcc" "CMakeFiles/main.dir/src/greedy.cpp.o.d"
+  "/home/paubover/Escritorio/2dCuatri/Practicas_mh/src/pincrem.cpp" "CMakeFiles/main.dir/src/pincrem.cpp.o" "gcc" "CMakeFiles/main.dir/src/pincrem.cpp.o.d"
+  "/home/paubover/Escritorio/2dCuatri/Practicas_mh/src/randomsearch.cpp" "CMakeFiles/main.dir/src/randomsearch.cpp.o" "gcc" "CMakeFiles/main.dir/src/randomsearch.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
